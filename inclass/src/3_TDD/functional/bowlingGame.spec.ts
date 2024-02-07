8a6546df3ea2c53681d3c869b9c4300afc670ec1
Patricio P <patriciopenaojeda@gmail.com>
2024-02-07 17:18:41 -0600

    Tarea

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,66 @@
+//Necesario --> npm i --save-dev @types/jest
+
+import { createBowlingGame } from "./bowlingGame";
 
 describe('Test bowling game', () => {
+  let game: ReturnType<typeof createBowlingGame>;
 
-  it('dummy test', () => {
-    expect(1).toBe(1)
-  })
+  beforeEach(() => {
+    game = createBowlingGame().resetState();
+  });
+  
+  it('should allow rolling a ball', () => {
+    expect(game.roll(3)).toBeDefined();
+  });
 
+  it('Should roll a gutter game', () => {
+    let rolls = 20;
+    rollMany(rolls, game, 0); 
+    expect(game.getScore()).toBe(0);
+  });
+
+  it('Should roll all ones', () => {
+    let rolls = 20;
+    rollMany(rolls, game, 1);
+    expect(game.getScore()).toBe(20);
+  });
+
+  it('Should roll a spare', () => {
+    rollSpare(game); 
+    rollMany(17, game, 0); 
+    expect(game.getScore()).toBe(16);
+  });
+
+  it('Should roll a strike', () => {
+    rollStrike(game); 
+    rollMany(17, game, 0); 
+    expect(game.getScore()).toBe(22);
+  });
+
+  it('Should roll a perfect game', () => {
+    rollMany(12, game, 10); 
+    expect(game.getScore()).toBe(300);
+  });
 });
+
+
+function rollMany(rolls: number, game: ReturnType<typeof createBowlingGame>, pins: number) {
+  for (let i = 0; i < rolls; i++) {
+    game = game.roll(pins);
+  }
+}
+
+function rollStrike(game: ReturnType<typeof createBowlingGame>){
+  game = game.roll(10);
+  game = game.roll(3);
+  game = game.roll(3);
+  return game;
+}
+
+function rollSpare(game: ReturnType<typeof createBowlingGame>){
+  game = game.roll(5);
+  game = game.roll(5);
+  game = game.roll(3);
+  return game;
+}
+
--- inclass/src/3_TDD/functional/bowlingGame.ts
+++ inclass/src/3_TDD/functional/bowlingGame.ts
@@ -11,12 +11,58 @@
  * to change the state var... just be carefull...⚠️
  */
 
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+  // Patricio A. Peña Ojeda
 
-export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-  //myFunction: () => callInnerModuleScopeFunction()
-  //myOhterFunction: (...) => myOtherFunction(...)
-})
+  type State = { rolls: number[] };
 
-//const myOtherFunction = (...) => {...}
+  const inState: State = { rolls: [] };
+  const frm: number = 10;
+  
+  export const createBowlingGame = (state: State = inState) => {
+    const roll = (pins: number) => rollHelper(state, pins);
+    const getScore = () => calculateScore(state); 
+    const resetState = () => createBowlingGame();
+  
+    const rollHelper = (state: State, pins: number) => {
+      const rolls = [...state.rolls, pins];
+      return createBowlingGame({ rolls });
+    };
+  
+    const calculateScore = (state: State) => {
+      let score = 0;
+      let firstTry = 0;
+  
+      for (let frame = 0; frame < frm; frame++) {
+        if (isStrike(state, firstTry)) {
+          score += scoreForStrike(state, firstTry);
+          firstTry++;
+        } else if (isSpare(state, firstTry)) {
+          score += scoreForSpare(state, firstTry);
+          firstTry += 2;
+        } else {
+          score += state.rolls[firstTry] + state.rolls[firstTry + 1];
+          firstTry += 2;
+        }
+      }
+  
+      return score;
+    };
+
+    const isSpare = (state: State, firstTry: number) =>
+      state.rolls[firstTry] + state.rolls[firstTry + 1] === 10;
+    const isStrike = (state: State, firstTry: number) =>
+      state.rolls[firstTry] === 10;
+  
+    const scoreForSpare = (state: State, firstTry: number) =>
+      10 + state.rolls[firstTry + 2];
+    const scoreForStrike = (state: State, firstTry: number) =>
+      10 + state.rolls[firstTry + 1] + state.rolls[firstTry + 2];
+  
+    return Object.freeze({
+      roll,
+      getScore,
+      resetState,
+    });
+  };
+
+  
